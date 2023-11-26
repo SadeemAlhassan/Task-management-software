b0294ad6c18755afd16b06e33bfa66544a6ee3e0
aruba6926 <152025341+aruba6926@users.noreply.github.com>
2023-11-26 16:46:31 +0300

    Add files via upload

---
+++ app/src/main/java/net/penguincoders/doit/RecyclerItemTouchHelper.java
@@ -0,0 +1,61 @@
+package net.penguincoders.doit;
+
+import android.content.DialogInterface;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.RecyclerView;
+
+import net.penguincoders.doit.Adapters.ToDoAdapter;
+
+public class RecyclerItemTouchHelper extends ItemTouchHelper.SimpleCallback {
+
+    private ToDoAdapter adapter;
+
+    public RecyclerItemTouchHelper(ToDoAdapter adapter) {
+        super(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT | ItemTouchHelper.UP | ItemTouchHelper.DOWN);
+        this.adapter = adapter;
+    }
+
+    @Override
+    public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+        adapter.onItemMove(viewHolder.getAdapterPosition(), target.getAdapterPosition());
+        return true;
+    }
+
+    @Override
+    public void onSwiped(@NonNull final RecyclerView.ViewHolder viewHolder, int direction) {
+        final int position = viewHolder.getAdapterPosition();
+        if (direction == ItemTouchHelper.LEFT) {
+            Snackbar.make(viewHolder.itemView, "Are you sure you want to delete this task?", Snackbar.LENGTH_LONG)
+                    .setAction("Confirm", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            adapter.deleteItem(position);
+                        }
+                    })
+                    .setActionTextColor(Color.RED)
+                    .show();
+            adapter.notifyItemChanged(position);
+        } else {
+            adapter.editItem(position);
+        }
+    }
+
+    @Override
+    public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+        super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+
+        // Customize the visual presentation of swiping action
+        // ...
+
+    }
+}
+
